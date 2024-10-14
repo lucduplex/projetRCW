c06d03323301b000fc27dbbeaab8af29f998b51e
lucduplex <lucduplex@outlook.com>
2024-10-14 11:02:48 -0400

    update sprint1

--- easyride/easyride/settings.py
+++ easyride/easyride/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'app/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']  # Fixed the path for static files
 
 MEDIA_URL = '/media/'
